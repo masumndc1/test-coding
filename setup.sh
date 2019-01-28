4571a08b0b2b864c3d707e9216136775f45dbb87
mohammad khabir uddin <masumndc1@gmail.com>
2019-01-28 23:18:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo -e ""
-echo -e "setting up infra using ansible"
+echo -e "\tsetting up infra using ansible"
 echo -e ""
 ansible-playbook test.yaml -c local
  
-echo -e "there are two webservers is running"
-echo -e "one is nginx, running on 8081"
-echo -e "another is apache2, running on 8080"
+echo -e "\tthere are two webservers is running"
+echo -e "\tone is nginx, running on interface:8081"
+echo -e "\tanother is apache2, running on interface:8080"
 echo -e ""
