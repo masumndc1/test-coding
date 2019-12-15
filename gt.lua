67c39c9ba79943433a04d8f7b0e68387ab59ac21
Mohammad Khabir Uddin <masumndc1@gmail.com>
2019-12-16 00:21:56 +0200

    arg[1]

---
+++ gt.lua
@@ -0,0 +1,23 @@
+
+
+-- function for to print long string of star
+
+function star()
+  print(string.rep("*", 50))
+end
+
+star()
+print("Adding newly edited files")
+os.execute ("git add .")
+print("Commiting with messages: arg[1]")
+star()
+os.execute ("git commit -m arg[1]")
+star()
+print("Pulling new changes first")
+star()
+os.execute ("git pull")
+star()
+print("Pushing to upstream")
+star()
+os.execute("git push origin master")
+star()
