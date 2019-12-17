03081f4ff39c25dc9bd30e8445588831e31d8e90
Mohammad Khabir Uddin <masumndc1@gmail.com>
2019-12-17 23:44:40 +0200

    what

--- gt.lua
+++ gt.lua
@@ -5,17 +5,20 @@ function long_string()
   print(string.rep("-", 70))
 end
 
-function git_operation(msg)
+function git_operation()
   long_string()
   print("pulling down first")
 --  os.execute ('ping -c 10 ' .. msg)
   os.execute ('git add .')
   print("commiting with msg")
-  os.execute ('git commit -m ' .. msg )
+  os.execute ('git commit -m ' .. arg[1] )
   print("pushing now to upstream")
   os.execute ('git push origin master')
   long_string()
 end
 
-string=arg[1]
-git_operation(string)
+--[[ 
+ string=arg[1]
+ print(string)
+--]]
+git_operation()
