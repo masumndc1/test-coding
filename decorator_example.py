f2ac066a8c32bc20359b27b4757b5eb5c9bcd7f7
Mohammad Khabir Uddin <masumndc1@gmail.com>
2022-06-26 22:02:13 +0300

    decorator examples

---
+++ decorator_example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import logging
+import time
+
+def my_logger(orig_func):
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs)
+        )
+        return orig_func(*args, **kwargs)
+    
+    return wrapper
+    
+def my_timer(orig_func):
+
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in : {} sec'.format(orig_func.__name__, t2))
+        return result
+    
+    return wrapper
+
+@my_logger
+def display_info(name: str, age: int):
+    print("display_info ran with arguments {} {}". format(name, age))
+
+display_info('john', 25)
+display_info('hulk', 30)
