375d02cc18319a9809018942f1d25c1e76428750
Khabir Uddin <khuddin@csc.fi>
2022-06-29 18:28:23 +0300

    a simple unittest

--- testgenerator.py
+++ testgenerator.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 
 import unittest
-from generator import generator_practise
+from xml.dom import NotFoundErr
+from generator import GeneratorPractise
+#import generator
 
 class generatorExample(unittest.TestCase):
 
-	def test_num(self):
-		b = [1,4,9,16,25]
-		my_nums = generator_practise([1,2,3,4,5])
-		for num in my_nums:
-			self.assertIn(num, b)
+    def test_num(self):
+        out = [1,4,9,16,25]
+        my_nums = GeneratorPractise([1,2,3,4,5])
+        for num in my_nums.generator_list():
+            self.assertIn(num, out)
+    
+    def test_exist(self):
+        out = [1,4,9,16,25]
+        self.assertIn(7, out)
+
+    def test_1_cannot_add_int_and_str(self):
+        with self.assertRaises(TypeError):
+            1 + 1
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
