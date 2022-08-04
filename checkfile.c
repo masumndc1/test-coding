ff6259cfdfd878dd992db675749655f10e3e55a2
Khabir Uddin <khuddin@csc.fi>
2022-08-04 16:12:49 +0300

    how to check a file in c

--- checkfile.c
+++ checkfile.c
@@ -3,13 +3,22 @@
 int isValidFile(char* path)
 {
 	FILE* checker = fopen(path, "r");
-    if (checker)
-    {
-    	fclose(checker);
+    if (checker) {
+        fclose(checker);
         return 1;
     }
-    else
-    {
-    	return 0;
+    else {
+        return 0;
+    }
+}
+
+int 
+main(int argc, char **argv) {
+    int test=isValidFile(argv[1])
+    if(test) {
+        print("file exists")
+    }
+    else {
+        print("file does not exist")
     }
 }
