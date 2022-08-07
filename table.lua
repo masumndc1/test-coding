faa3e84e1e8091c86214d69bef0b09b0b0a3c127
Khabir Uddin <khuddin@csc.fi>
2022-08-08 01:49:02 +0300

    lua: tables and metatables practise1

--- table.lua
+++ table.lua
@@ -1,32 +1,50 @@
 -- lua table practise
 
-local t = {}
-for i = 1, 10, 1 do
-  table.insert(t, i*i)
-  -- t[i] = i*i
-end
---[[
-for i = 1, #t do
-  print(t[i])
+local me = {}
+--print("i have ", me.money)
+
+local rich = { money = 999999 }
+--print("rich has", rich.money)
+
+-- When rich give me some money
+local rich_donate = { __index = rich }
+setmetatable(me, rich_donate)
+
+-- print("After rich donate me i have ", me.money)
+
+Set = {}
+local mt = {}
+
+function Set.new(l)
+    local set = {}
+    setmetatable(set, mt)
+    for k,v in ipairs(l) do 
+       set[k] = v
+    end
+    return set
 end
 
-for k,v in ipairs(t) do
-  print(k,v)
+function Set.union (a,b)
+    local set = {}
+    for k,v in ipairs(a) do set[k] = v end
+    for k,v in ipairs(b) do set[#a + k] = v end
+    return set
 end
 
-local m = {'a', 'b', 'c'}
--- m = table.concat(m,",",2)
-for k,v in ipairs(m) do
-  print(k,v)
+function Set.print (l)
+    local set = {}
+    for k,v in ipairs(l) do set[k] = v end
+    return table.concat(set, ",")
 end
-]]
 
--- print all the arguments passed through the cli
 
-if #arg >= 1 then
-  for i = 1, #arg do
-     print(i,arg[i])
-   end
-end
-print(arg[0])
+a=Set.new({1,2,3})
+b=Set.new({4,5,6})
+print(Set.print(a) .. " + " .. Set.print(b))
 
+mt.__add = Set.union
+
+local c = a + b
+print(Set.print(c))
+
+-- end of file
