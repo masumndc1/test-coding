9356978db18bcdb49bf0f471aa7b19f679192d7d
Khabir Uddin <khuddin@csc.fi>
2022-08-09 10:11:04 +0300

    lua: globar variable

---
+++ globalVar.lua
@@ -0,0 +1,14 @@
+-- example of global var
+
+-- local _G = _G
+--local _ENV = _ENV
+
+-- for v in pairs(_G) do print(v) end
+--for v in pairs(_ENV) do print(v) end
+
+-- local value = _G[load]
+
+-- print(value)
+--
+print(os.getenv("HOME"))
+print(os.environ())
