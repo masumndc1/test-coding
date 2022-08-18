29a5d19c63ffd6b2b04d246185fc1edab0b316ca
Khabir Uddin <masumndc1@gmail.com>
2022-08-19 01:43:06 +0300

    lua: print line of dash with lightgreen

--- gt.lua
+++ gt.lua
@@ -1,6 +1,9 @@
 #!/usr/bin/env lua
 
 -- I want to print the output in colored font.
+-- local red = '\27[31m'
+-- local yellow = '\27[33m'
+local lightgreen = '\27[36m'
 local green = '\27[32m'
 local blue = '\27[34m'
 local purple = '\27[35m'
@@ -8,7 +11,7 @@ local white = '\27[37m'
 
 local long_string = function()
   local n = io.popen("tput cols")
-  print(string.rep("-", n:read()))
+  print(lightgreen .. string.rep("-", n:read()))
 end
 
 local git_operation = function(msg)
