1765b2ad1c1df7b4def9e5b12b926b091de2d92c
Khabir Uddin <khuddin@csc.fi>
2022-08-19 10:02:39 +0300

    lua: improvement of a logic

--- fromc.lua
+++ fromc.lua
@@ -3,6 +3,11 @@
 -- call a c function from a c file
 local _G = _G
 
-ping_me = function (num, add)
+local ping_me = function (num, add)
     os.execute("ping -c" .. " " .. num .. " " .. add)
 end
+
+local handle = io.popen("ls -la")
+local resp = handle:read('*')
+
+print(resp)
