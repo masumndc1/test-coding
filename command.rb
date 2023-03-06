4c3445ef32166adc4edfb012a0774d56af3401a2
Khabir Uddin <khuddin@csc.fi>
2023-03-06 23:37:08 +0200

    more python practise

--- command.rb
+++ command.rb
@@ -6,10 +6,11 @@
 #
 #!/usr/bin/ruby
 require 'colorize'
-#
+# #
 puts "Blue text".blue
 puts 'Bold cyan on blue text'.cyan.on_blue.bold
 puts "This is #{"fancy".red} text"
+
 
 
 output = `ping -c 4 yahoo.com`
--- decorator_function.py
+++ decorator_function.py
@@ -1,25 +1,33 @@
+#!/usr/bin/env python3
 
 from base64 import decode
 
 
-def outer_fuction():
-    message = 'Hi'
-#!/usr/bin/env python3
+def outer_fuction():  # pylint disable=missing-function-or-method-docstring
+    message = "Hi"
 
 
 def decorated_function(original_function):
     def wrapper_function(*args, **kwargs):
-        print("wrapper executed this before {} function".format(original_function.__name__))
+        print(
+            "wrapper executed this before {} function".format(
+                original_function.__name__
+            )
+        )
         return original_function(*args, **kwargs)
+
     return wrapper_function
+
 
 @decorated_function
 def display():
     print("display function run")
 
+
 @decorated_function
 def display_info(name: str, age: int):
-    print("display_info ran with arguments {} {}". format(name, age))
+    print("display_info ran with arguments {} {}".format(name, age))
+
 
 display()
-display_info('John', 25)
+display_info("John", 25)
