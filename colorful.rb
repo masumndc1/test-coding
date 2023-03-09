4d27f7e6b7f7d60fcdbb26f9bd9ad14a18fcad07
Khabir Uddin <khuddin@csc.fi>
2023-03-09 23:51:37 +0200

    colorful text in ruby

---
+++ colorful.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require('colorize')
+
+def show_color(names)
+  names.each do |name|
+    if name.end_with?('c')
+      puts name.red
+    elsif name.end_with?('lua')
+      puts name.cyan
+    elsif name.end_with?('py')
+      puts name.blue
+    elsif name.end_with?('rb')
+      puts name.green
+    else
+      puts name.yellow
+    end
+  end
+end
