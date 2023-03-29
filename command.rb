bb5a425e32d75ed84add4a9a5f70e1a4dab17d08
Khabir Uddin <khuddin@csc.fi>
2023-03-29 18:17:52 +0300

    ruby: introducing optparse

--- command.rb
+++ command.rb
@@ -1,6 +1,25 @@
 #!/usr/env/ruby
 
+require('optparse')
 require_relative('execute')
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = %(
+  Usage: command.rb [options]
+  - c    --command    command to pass
+  - h    --help       show help menu
+  )
+
+  opts.on('-c', '--command') do |c|
+    options[:command] = c
+  end
+
+  opts.on('-h', '--help') do |h|
+    options[:help] = h
+    puts opts.banner
+  end
+end.parse!
 
 def main
   cmd = ARGV[0]
