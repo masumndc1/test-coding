de0b3729f5326a28371aca95945ebc7cf598a84c
Khabir Uddin <khuddin@csc.fi>
2023-05-19 16:54:01 +0300

    gt.py color in class

--- gt.py
+++ gt.py
@@ -8,22 +8,64 @@ import sys
 import subprocess
 
 
-class GitOperation():
+class Color():
+    """will give color"""
+
+    def __init__(self):
+        self.black = '\033[30m'
+        self.red = '\033[31m'
+        self.green = '\033[32m'
+        self.yellow = '\033[33m'
+        self.blue = '\033[34m'
+        self.megenta = '\033[35m'
+        self.cyan = '\033[36m'
+        self.white = '\033[37m'
+        self.underline = '\033[4m'
+        self.reset = '\033[0m'
+
+    def color(self, color):
+        if color == 'black':
+            return self.black
+        elif color == 'red':
+            return self.red
+        elif color == 'green':
+            return self.green
+        elif color == 'yellow':
+            return self.yellow
+        elif color == 'blue':
+            return self.blue
+        elif color == 'magenta':
+            return self.magenta
+        elif color == 'cyan':
+            return self.cyan
+        elif color == 'white':
+            return self.white
+        elif color == 'underline':
+            return self.underline
+        elif color == 'reset':
+            return self.reset
+        else: 
+            print("unknown color")
+
+
+class GitOperation(Color):
+
     '''
     gt.py "commit_msg" branch_name(optional) default_branch: master
     '''
 
     def __init__(self):
+        super().__init__()
         self.msg = sys.argv[1]
         self.branch = sys.argv[2] if len(sys.argv) == 3 else "master"
         self._add_new_files()
 
-    def _term_size(self):
+    def _term_size(self, color):
         col, _ = shutil.get_terminal_size()
-        print("-" * col)
-
+        print(color + "-" * col + self.reset)
+ 
     def _add_new_files(self):
-        self._term_size()
+        self._term_size(self.yellow)
         retcode = subprocess.call("git add .", shell=True)
         self._commit() if not retcode else sys.exit("could not add files")
 
@@ -37,7 +79,7 @@ class GitOperation():
             'git push origin "%s"' % self.branch, shell=True)
         if not retcode:
             print(f"Pushed to {self.branch} branch")
-            self._term_size()
+            self._term_size(self.cyan)
         else:
             sys.exit("could not push")
 
