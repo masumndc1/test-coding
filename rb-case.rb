2f37f1094ce4dbecc4aec6463ff1b9430614c6df
Khabir Uddin <khuddin@csc.fi>
2023-06-08 19:19:55 +0300

    ruby: install by case

--- rb-case.rb
+++ rb-case.rb
@@ -1,15 +1,31 @@
 #!/usr/bin/env ruby
 
-puts RUBY_PLATFORM
+def find_pkg
+  pkg = case RUBY_PLATFORM
+        when /[Dd]arwin/
+          'brew'
+        when /[Ll]inux/
+          'yum' if File.exist?('/usr/bin/yum')
+          'zypper' if File.exist?('/usr/bin/zypper')
+          'apt-get' if File.exist?('/usr/bin/apt')
+        end
+end
 
-pkg = case RUBY_PLATFORM
-      when /[Dd]arwin/
-        'brew'
-      when /[Ll]inux/
-        'yum' if File.exist?('/usr/bin/yum')
-        'zypper' if File.exist?('/usr/bin/zypper')
-        'apt' if File.exist?('/usr/bin/apt')
-      end
+def pkg?(pkg_name)
+  return true if File.exist?("/usr/bin/#{pkg_name}")
 
-puts pkg
-puts `#{pkg} list neovim`
+  false
+end
+
+def install_pkg(pkg, pkg_name)
+  `sudo #{pkg} install -y #{pkg_name}`
+end
+
+def main
+  raise 'exit' unless ARGV[0]
+
+  pkg_name = ARGV[0] if ARGV[0]
+  install_pkg(find_pkg, pkg_name) unless pkg?(pkg_name)
+end
+
+main if __FILE__ == $0
