50b5a68441b36705306a4758192e13d9607f6ca6
khabiruddin <khabir.uddin@csc.fi>
2023-08-19 01:24:34 +0300

    lua: color from color-module

---
+++ color.lua
@@ -0,0 +1,12 @@
+#!/usr/bin/env lua
+
+local color = {}
+
+color.yellow = "\27[33m"
+color.lightgreen = "\27[36m"
+color.green = "\27[32m"
+color.blue = "\27[34m"
+color.purple = "\27[35m"
+color.white = "\27[37m"
+
+return color
--- gt.lua
+++ gt.lua
@@ -1,34 +1,28 @@
 #!/usr/bin/env lua
 
 -- I want to print the output in colored font.
--- local red = '\27[31m'
-local yellow = "\27[33m"
-local lightgreen = "\27[36m"
-local green = "\27[32m"
-local blue = "\27[34m"
-local purple = "\27[35m"
-local white = "\27[37m"
+local color = require("color")
 
 local long_string = function()
 	local n = io.popen("tput cols")
-	print(lightgreen .. string.rep("-", n:read()))
+	print(color.lightgreen .. string.rep("-", n:read()))
 end
 
 local git_operation = function(msg)
 	long_string()
 	os.execute("git add .")
-	print(green .. "Commiting with msg '" .. msg .. white)
+	print(color.green .. "Commiting with msg '" .. msg .. color.white)
 	os.execute("git commit -m '" .. msg .. "' ")
-	print(purple .. "Pulling down now" .. white)
+	print(color.purple .. "Pulling down now" .. color.white)
 	os.execute("git pull --rebase")
-	print(blue .. "Pushing now to master" .. white)
+	print(color.blue .. "Pushing now to master" .. color.white)
 	os.execute("git push origin master")
 	long_string()
 end
 
 local main = function()
 	if #arg < 1 then
-		print(color.yellow .. 'usage: ./gt.lua "msg"' .. white)
+		print(color.yellow .. 'usage: ./gt.lua "msg"' .. color.white)
 		os.exit()
 	end
 
