ea2e475b4188bcfba9a8a46083543ef3d90c7f3f
khabiruddin <khabir.uddin@csc.fi>
2023-08-21 20:29:09 +0300

    lua: more vars for lua

--- vars.lua
+++ vars.lua
@@ -3,11 +3,13 @@
 local vars = {}
 
 vars.site = "www.google.com"
+vars.initial = 0
 vars.num = 5
 vars.nm_ping = 3
 vars.os = "port"
 vars.pkg = "htop"
 vars.term = os.getenv("TERM")
 vars.shell = os.getenv("SHELL")
+vars.list_cur_dir = "ls ."
 
 return vars
