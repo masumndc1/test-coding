be64e1061573487b7f14a80ba34acac70d99d2cf
khabiruddin <khabir.uddin@csc.fi>
2023-08-21 23:03:05 +0300

    lua: run command and return value

--- commands.lua
+++ commands.lua
@@ -13,11 +13,45 @@ function commands.find_pkg()
 		return "yum"
 	elseif io.open("/usr/bin/zypper", "r") then
 		return "zypper"
+	elseif io.open("/opt/homebrew/bin/brew", "r") then
+		return "brew"
+	elseif io.open("/opt/local/bin/port", "r") then
+		return "port"
+	else
+		return false
 	end
 end
 
 function commands.os_path_exists(file)
 	if io.open(file, "r") then
+		return true
+	else
+		return false
+	end
+end
+
+function commands.os()
+	local os_name = io.popen("uname -o")
+	if os_name then
+		if os_name:read() == "Darwin" then
+			return "mac"
+		elseif os_name:read() == "Linux" then
+			return "linux"
+		end
+	else
+		print("could not determine os")
+		return false
+	end
+end
+
+function commands.run_command(cmd)
+	-- this will return true or false as exist code
+	-- based on successful execution
+	local cmds = assert(io.popen(cmd))
+	if cmds:read() then
+		for line in cmds:lines() do
+			print(line)
+		end
 		return true
 	else
 		return false
