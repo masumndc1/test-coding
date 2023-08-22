b0e572e521bed75432fda771b8ea0c1c2edeb0de
khabiruddin <khabir.uddin@csc.fi>
2023-08-22 14:46:05 +0300

    lua: give some title

--- sorted_file.lua
+++ sorted_file.lua
@@ -4,6 +4,7 @@ local sorted_file = function(file)
 	local srtd_file = {}
 
 	-- show file content before sorted
+	print("\n" .. "--- before sort ---")
 	local fl = assert(io.open(file, "r"))
 	print(fl:read("a"))
 
@@ -16,6 +17,7 @@ local sorted_file = function(file)
 	table.sort(srtd_file)
 
 	-- now list the sorted table content
+	print("--- sorted ---")
 	for _, v in pairs(srtd_file) do
 		print(v)
 	end
