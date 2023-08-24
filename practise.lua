eb98e4db7600b09baf0b42de81c712d73f9c927c
khabiruddin <khabir.uddin@csc.fi>
2023-08-24 13:34:53 +0300

    lua: they are just some practise

--- practise.lua
+++ practise.lua
@@ -5,58 +5,22 @@ local commands = require("commands")
 local vars = require("vars")
 local longest_line = require("longest_line")
 
---[[
+--[[ list all vars from global G
 for n in pairs(_G) do
 	print(n)
 end
 
--- [[local operations = require "operations"
--- this is a comment
--- this is another line of comment
--- this is the i like more
---
--- multiple lines of code is
--- written like bellow
---
---[[
+-- example of string.format
 local pkg_comm = [[
-    sudo %s update; sudo %s search %s
-
-os.execute((string.format(pkg_comm, vars.os, vars.os, vars.pkg)))
+  "sudo %s update; sudo %s search %s"
 ]]
+-- os.execute((string.format(pkg_comm, vars.os, vars.os, vars.pkg)))
 
 --[[
-a = {} -- empty table
-for i = 1, 10 do
-	a[i] = i
-end
-
-for k, v in pairs(a) do
-	print(k, v)
-end
--- an example of while
-
-for k, v in ipairs(a) do
-	print(k, v)
-end
-]]
---[[
-local num = 1
-while num < 10 do
-	print(num)
-	num = num + 1
-end
-
-for i = 1, 100, 5 do
-	if (i % 2) == 0 then
-		print(i)
-	end
-end
-
---[[
-function Increment(...) -- body
+-- look at here how lua uses positional arguments and and how it uses
+function Increment(...)           -- body
 	local result = 0
-	for _, a in next, { ... } do
+	for _, a in next, { ... } do    -- ... become table here
 		result = a + result
 	end
 	return result
@@ -64,22 +28,11 @@ end
 
 Increment(1, 2)
 
+-- using of assert is always good.
 if assert(os.execute("ls" .. " -la")) then
 	print(color.blue .. "command executation was good" .. color.white)
 end
---]]
-
---[[
-local code, command, integer = os.execute("ls /usr/bin/env")
-if code then
-	print(code)
-	print(command)
-	print(integer)
-end
---]]
--- print(code)
-
--- if file_exists(arg[1]) then print("this file exists") else print("does not exists") end
+]]
 
 local lua_file = assert(io.popen(vars.list_cur_dir))
 local i = vars.initial
@@ -91,26 +44,15 @@ for line in lua_file:lines() do
 end
 print(string.format("number of lua files found %d", i))
 
---operations.fileexists(arg[1])
---operations.runcommand("ping -c 2 yahoo.com")
 assert(type(vars.site) == "string", "pass string value")
 assert(type(vars.nm_ping) == "number", "pass number")
 -- commands.ping(vars.nm_ping, vars.site)
 
 -- print(commands.os_path_exists("/usr/bin/apt"))
-print(vars.term)
+-- what is my terminal
+-- print(vars.term)
 
-local shell = function()
-	local shell = nil
-	if vars.shell ~= "zsh" then
-		shell = "zsh"
-	else
-		shell = "bash"
-	end
-	return shell
-end
-
-print(shell())
+print(vars.shell)
 print(commands.find_pkg())
 print(commands.os())
 local ret = commands.run_command("ls -la")
@@ -118,6 +60,7 @@ if ret then
 	print("command run successfully")
 end
 
-local long_nm, line = longest_line.nm_line("sort.lua")
+local file = arg[1] or "sort.lua"
+local long_nm, line = longest_line.nm_line(file)
 print(line)
-print(type(long_nm))
+print(long_nm)
