20ef4c394bf624056acb5f2d154a362d5d9d69c3
Khabir Uddin <masumndc1@gmail.com>
2023-10-06 00:44:19 +0300

    python: decorator

--- py-decorator_basic.py
+++ py-decorator_basic.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
-from __future__ import annotations
-
-import functools
+from functools import wraps
 
 
 def return_2(func):
+    @wraps(func)
     def decorator():
         print("return 2")
         func()
-        return decorator
-
-    return return_2
+    return decorator
 
 
 @return_2
@@ -19,7 +16,9 @@ def return_3():
     print("something")
 
 
-# return_3()
-# return_2(return_3())
+def main():
+    return_3()
 
-# print(return_3())
+
+if __name__ == "__main__":
+    main()
--- py-decorator_class.py
+++ py-decorator_class.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
 
 class decorator_class(object):
-    
+
     def __init__(self, original_function):
         self.original_function = original_function
 
     def __call__(self, *args: any, **kwargs: any):
-        print("call methods executed this before {} ".format(self.original_function.__name__))
+        print("call methods executed this before {} ".format(
+            self.original_function.__name__))
         return self.original_function(*args, **kwargs)
+
 
 @decorator_class
 def display():
     print("display function run")
 
+
 @decorator_class
 def display_info(name: str, age: int):
     print("display_info ran with arguments {} {}". format(name, age))
+
 
 display()
 display_info('John', 25)
